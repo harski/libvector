f8729b686b4c1bd07e0e190c55127078072fb77e
Tuomo Hartikainen <hartitu@gmail.com>
2012-10-13 01:05:48 +0300

    Document the functions in vector.h

--- src/vector.h
+++ src/vector.h
@@ -26,15 +26,47 @@ struct vector {
 };
 
 
+/* Add element to vector v.
+ * Returns the number of elements in vector v.*/
 int vector_add (struct vector *v, void *element);
+
+/* Add the elements in v2 to v1. v2 is left intact.
+ * Returs the number of elements in v1.*/
 int vector_add_vector (struct vector *v1, struct vector *v2);
+
+/* Initialize a vector with an initial capacity of VECTOR_DEFAULT_SIZE.
+ * Returns the allocated, initialized vector, or if an error occured NULL. */
 struct vector * vector_init ();
+
+/* Initialize a vector with an initial capacity defined with size.
+ * Returns the allocated, initialized vector, or if an error occured NULL.. */
 struct vector * vector_init_size (size_t size);
+
+/* Initialize a vector with an initial capacity of VECTOR_DEFAULT_SIZE.
+ * Returns the allocated, initialized vector, or if an error occured NULL..
+ * NOTE: THIS FUNCTION IS DEPRECATED. USE vector_init() INSTEAD. */
 DEPRECATED(struct vector * vector_create ());
+
+/* Initialize a vector with an initial capacity defined with size.
+ * Returns the allocated, initialized vector, or if an error occured NULL..
+ * NOTE: THIS FUNCTION IS DEPRECATED. USE vector_init_size() INSTEAD. */
 DEPRECATED(struct vector * vector_create_size (size_t size));
+
+/* Free vector v. v must be manually emptied first, this function doesn't
+ * free the elements that might still be remaining in the vector. */
 void vector_destroy (struct vector *v);
+
+/* Get element specified by index from the vector v.
+ * Returns the element in the index if such exists, otherwise NULL. */
 void * vector_get (const struct vector *v, unsigned int index);
+
+/* Remove element from index specified by the "index" parameter from the vector
+ * v and return it to the caller.
+ * Returns the element at the index "index", or NULL if no such element
+ * exists. */
 void * vector_remove (struct vector *v, unsigned int index);
+
+/* Compacts the pointer array in vector v to be as small as possible. */
 void vector_compact (struct vector *v);
 
 #endif /* VECTOR_H */
