harski/libvector

commit c3e47b1023e543ef103e7e8d8f18f19e88fea7b0

Use a function for resizing the array

src/vector.c
@@ -4,19 +4,26 @@
 #include "vector.h"
 
 
+static int vector_resize (struct vector *v, size_t size)
+{
+    if (size > 0) {
+        void * tmp = malloc(size*sizeof(void *));
+        if (tmp==NULL) return 0;
+
+        memcpy(tmp, v->list, v->elements*sizeof(void*));
+        free(v->list);
+        v->list = tmp;
+    }
+
+    return size;
+}
+
+
 int vector_add (struct vector *v, void *element)
 {
     /* If list too small, expand */
     if(v->elements == v->size) {
-        void * tmp = malloc(2 * v->size * sizeof(void *));
-        if (tmp == NULL)
-            return 0;
-
-        v->size *= 2;
-        memcpy(tmp, v->list, v->elements*sizeof(void *));
-
-        free(v->list);
-        v->list = tmp;
+        if(!vector_resize(v, 2*v->size)) return 0;
     }
 
     v->list[v->elements] = element;

commit 5b7b9e0fd436432e12c889c44252b7bc82cb1814

Fix: update list size when resizing list
Commit c3e47b1 broke this.

src/vector.c
@@ -11,12 +11,15 @@
 static int vector_resize (struct vector *v, size_t size)
 {
     if (size > 0) {
-        void * tmp = malloc(size*sizeof(void *));
-        if (tmp==NULL) return 0;
+        void ** tmp = (void **) malloc(size*sizeof(void *));
+        if (tmp==NULL)
+            return 0;
 
         memcpy(tmp, v->list, v->elements*sizeof(void*));
+
         free(v->list);
         v->list = tmp;
+        v->size = size;
     }
 
     return size;
@@ -62,7 +65,7 @@ struct vector * vector_init_size (size_t size)
     if (v==NULL)
         return NULL;
 
-    v->list = malloc(size*sizeof(void *));
+    v->list = (void **) malloc(size*sizeof(void *));
 
     v->size = size;
     v->elements = 0;